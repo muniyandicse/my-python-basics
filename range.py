b54c138b44b0c530d88a79d192b86a6a99b1d80e
Muniyandi <108913062+muniyandicse@users.noreply.github.com>
2022-08-04 19:58:10 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,40 @@
+for i in range(5):
+    for j in range(i,5):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print(" ")
+print("-----------------")
+for i in range(5):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,5):
+        print("*",end=" ")
+    print(" ")
+print("-----------------")
+for i in range(5):
+    for j in range(i+1):
+        print("*",end=" ")
+    print(" ")
+print("-----------------")
+for i in range(5,0,-1):
+    for j in range(i+1):
+        print("*",end=" ")
+    print(" ")
+print("-----------------")
+for i in range(5):
+    for j in range(i+1):
+        print("*",end=" ")
+    for i in range(5):
+        for j in range(i, 5):
+            print(" ", end=" ")
+        for j in range(i + 1):
+            print("*", end=" ")
+        print(" ")
+
+
+
+
+
+
+
